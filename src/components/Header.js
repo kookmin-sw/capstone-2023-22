a5e6c8c85ddeeeb128762d0dfb675c3212d730ad
KIM JINJAE <jinjae.dev@gmail.com>
2023-03-11 16:11:31 +0900

    fix: logo image render

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,13 +2,14 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import './Header.css';
+import mainlogo from '../images/logo.png';
 
 function Header() {
   return (
     <div>
       <Link to="/" style={{textDecoration: "none"}}>
         <div className='Home-header'>
-            <img className='Home-logo' alt="logo" src="logo.png" />
+            <img className='Home-logo' alt="logo" src={mainlogo} />
             <div className='Home-header-title'>
             <h2 className='Home-header-title-explanation'>세상의 소소한 문화행복 생활을 즐기다,</h2>
             <h1 className='Home-header-title-name'>세소행</h1>

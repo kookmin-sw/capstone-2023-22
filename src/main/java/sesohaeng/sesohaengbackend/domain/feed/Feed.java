38038cc67a4a3c33c2e124df43ebba9dc101dad6
yuujinkim <66306573+yuujinkim@users.noreply.github.com>
2023-03-23 13:59:59 +0900

    fix: 4차 DB에 따른 entity 수정

--- src/main/java/sesohaeng/sesohaengbackend/domain/feed/Feed.java
+++ src/main/java/sesohaeng/sesohaengbackend/domain/feed/Feed.java
@@ -1,9 +1,11 @@
 package sesohaeng.sesohaengbackend.domain.feed;
 
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import sesohaeng.sesohaengbackend.domain.BaseTimeEntity;
 import sesohaeng.sesohaengbackend.domain.feedimage.FeedImage;
 import sesohaeng.sesohaengbackend.domain.like.Like;
+import sesohaeng.sesohaengbackend.domain.place.Place;
 import sesohaeng.sesohaengbackend.domain.user.User;
 
 import javax.persistence.*;
@@ -11,16 +13,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-@Table(name = "feed")
+@Getter
 @NoArgsConstructor
+@Table(name = "FEED")
 public class Feed extends BaseTimeEntity {
-
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-
-    @Column
-    private String placeName;
 
     @Column
     private String content;
@@ -29,11 +28,13 @@ public class Feed extends BaseTimeEntity {
     @JoinColumn(name = "user_id")
     private User user;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "place_id")
+    private Place place;
+
     @OneToMany(mappedBy = "feed", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<FeedImage> images = new ArrayList<>();
 
     @OneToMany(mappedBy = "feed", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Like> likes = new ArrayList<>();
-
-
 }

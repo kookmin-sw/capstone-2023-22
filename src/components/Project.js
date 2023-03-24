0898567e66275bf149a0bb527260b399b1c1c076
jin-jae <jinjae.dev@gmail.com>
2023-03-24 12:29:17 +0900

    add: about-project page main text

--- src/components/Project.js
+++ src/components/Project.js
@@ -6,12 +6,29 @@ import StyledButton from "./StyledButtton";
 function Project() {
   return (
     <div className="Project">
-      <h1>Project Source Code</h1>
-      <StyledButton text="Source Code 링크" link="https://github.com/kookmin-sw/capstone-2023-22"/>
-      <img alt="github opened issues" src="https://img.shields.io/github/issues-raw/kookmin-sw/capstone-2023-22?style=for-the-badge&color=green" />
-      <img alt="github closed issues" src="https://img.shields.io/github/issues-closed-raw/kookmin-sw/capstone-2023-22?style=for-the-badge&color=red" />
-      <img alt="github opened pull requests" src="https://img.shields.io/github/issues-pr-raw/kookmin-sw/capstone-2023-22?style=for-the-badge&color=green" />
-      <img alt="github closed pull requests" src="https://img.shields.io/github/issues-pr-closed-raw/kookmin-sw/capstone-2023-22?style=for-the-badge&color=red" />
+      <div style={{display: "flex", alignItems: "center"}}>
+        <h1>프로젝트 소개</h1>
+        &nbsp;&nbsp;&nbsp;&nbsp;
+        <StyledButton text="Source Code 링크" link="https://github.com/kookmin-sw/capstone-2023-22"/>
+      </div>
+      <h2>세소행: "세상의 소소한 문화행복 생활을 즐기다"</h2>
+      <p className="main-text">
+        2022년 10월 29일 발생한 이태원 사건은 사회적으로 큰 충격을 불러일으켰으며, 동시에 "인구밀집"을 중요한 사회적 키워드로 부각시켰습니다. <br />
+        <a href="https://www.hani.co.kr/arti/society/society_general/1069820.html">‘이태원 참사’ 트라우마 83%가 2030…또래 희생 충격 컸나</a> <br />
+        저희는 이러한 문제점으로부터 '만약 특정 지역의 인구 밀집 정도를 예측할 수 있고, 실제 인구밀집이 발생하는 이유를 확인할 수 있다면 사회적 문제의 해결로도 이어질 수 있지 않을까?'라고 생각하였습니다.
+        이러한 인구밀집 현상을 '트렌드'의 측면에서 바라보고, 문화 생활을 즐길 수 있는 장소에 대한 정보를 추가적으로 소비자에게 제공하여 보다 다양한 장소에서 문화생활을 즐길 수 있도록 해당 애플리케이션 서비스를 기획하였습니다.
+      </p>
+      <h1>기술 스택</h1>
+      <p>서비스 개발 과정에서 저희가 사용한 주요 기술 스택은 다음과 같습니다.</p>
+      <table>
+        <thead><th>PART</th><th>STACKS</th></thead>
+        <tr><td>Frontend</td><td><img alt="React Native Logo" src="https://img.shields.io/badge/React Native-61DAFB?style=flat-square&logo=React&logoColor=black"/></td></tr>
+        <tr><td>Backend</td><td><img alt="Spring" src="https://img.shields.io/badge/Spring-6DB33F?style=flat-square&logo=Spring&logoColor=white"/></td></tr>
+        <tr><td>MLOps</td><td></td></tr>
+        <tr><td>Cloud</td><td><img alt="AWS" src="https://img.shields.io/badge/Amazon AWS-232F3E?style=flat-square&logo=amazonaws&logoColor=white"/></td></tr>
+      </table>
+      <h1>시스템 구조</h1>
+      <p>(추가 예정입니다.)</p>
     </div>
   );
 }
--- src/components/Team.js
+++ src/components/Team.js
@@ -1,19 +1,22 @@
 import React from "react";
+import { ReactMarkdown } from "react-markdown/lib/react-markdown";
 import StyledButton from "./StyledButtton";
 
 import "./Team.css";
 
-function PersonInfo( { image, name, sn, githubLink, part } ) {
+function PersonInfo( { image, name, sn, githubLink, part, message } ) {
   return (
-    <div className="Person-info">
+    <div style={{display: "flex", flexDirection: "row"}}>
       <img className="Profile-image" alt={`${name}'s profile`} src={image} />
-      <div className="Person-name">{name}</div>
-      <div className="Person-details">
-        <div className="Person-details-self">{sn}</div>
-        <div className="Person-details-self"><StyledButton text="Github" link={githubLink}/></div>
-        <div className="Person-details-self">{part}</div>
+      <div className="Person-info">
+        <div className="Person-info-main">
+          <div className="Person-represent">{name} ({sn})</div>
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          <StyledButton text="Github" link={githubLink}/>
+        </div>
+        <div className="Person-details-part">{part}</div>
+        <blockquote className="Person-details-quote"><ReactMarkdown children={message} /></blockquote>
       </div>
-
     </div>
     );
 }
@@ -26,7 +29,10 @@ function Team() {
         name="김진재"
         sn="****2648"
         githubLink="https://github.com/jin-jae"
-        part="팀장, 디자인, Frontend"
+        part="팀장, 디자인(기획), Frontend"
+        message={`안녕하세요, 다양성의 가치를 추구하는 개발자 **김진재**입니다.
+
+넓고 깊은 **탐색가의 삶**을 지향합니다 :)`}
       />
       <PersonInfo
         image="https://avatars.githubusercontent.com/u/66306573?v=4"
@@ -34,14 +40,15 @@ function Team() {
         sn="****3044"
         githubLink="https://github.com/yuujinkim"
         part="Backend (Logic)"
+        message={`안녕하세요. 꾸준한 학습과 성장을 추구하는 김유진입니다. 함께 성장해가는 개발 문화를 지향합니다.`}
       />
       <PersonInfo
         image="https://avatars.githubusercontent.com/u/79959576?v=4"
         name="박세열"
         sn="****2648"
         githubLink="https://github.com/SeYeol00"
-        part="Backend
-        (Security, Infrastructure)"
+        part="Backend (Security, Infrastructure)"
+        message={`안녕하세요, 박세열입니다.`}
       />
       <PersonInfo
         image="https://avatars.githubusercontent.com/u/105899098?v=4"
@@ -49,13 +56,19 @@ function Team() {
         sn="****1665"
         githubLink="https://github.com/Halfmbbn"
         part="Frontend"
+        message={`안녕하세요. 기초가 튼튼한 개발자를 꿈꾸는 이상현입니다.`}
       />
       <PersonInfo
         image="https://avatars.githubusercontent.com/u/80037682?v=4"
         name="평선호"
         sn="****1703"
         githubLink="https://github.com/sunho1999"
-        part="AI"
+        part="MLOps"
+        message={`현재 99년생 18학번 소프트웨어학부 재학중인 평선호 라고 합니다.
+
+**배우는것을 공유하고 나누는것**이 **공부의 이상**이라 생각합니다.
+
+감사합니다 :-)`}
       />
     </div>
   );

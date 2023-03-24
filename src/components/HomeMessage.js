a2a04812ecd840573e6292a524d909e31370d967
jin-jae <jinjae.dev@gmail.com>
2023-03-24 12:30:35 +0900

    fix: design

--- src/components/HomeMessage.js
+++ src/components/HomeMessage.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import "./HomeMessage.css";
+import mainLogo from '../images/logo.png'
 
 
 function HomeMessage() {
   return (
     <div className='Main-message'>
-      <h1>세상의 소소한 문화행복 생활을 즐기다, 세소행</h1>
+      <img style={{height: "50vmin", borderRadius: "2rem", marginTop: "1rem"}} alt="sesohaeng-logo" src={mainLogo} />
+      <h1>"세상의 소소한 문화행복 생활을 즐기다, 세소행"</h1>
     </div>
   );
 }

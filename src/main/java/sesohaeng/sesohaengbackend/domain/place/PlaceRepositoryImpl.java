8ddf3cba2d1f0cbe5279b2cae0e6d1dd24ad340c
SeYeol00 <dmot0126@gmail.com>
2023-04-06 19:25:11 +0900

    add: 검색한 place의 id를 통해 플레이스의 정보를 가져오는 로직

--- src/main/java/sesohaeng/sesohaengbackend/domain/place/PlaceRepositoryImpl.java
+++ src/main/java/sesohaeng/sesohaengbackend/domain/place/PlaceRepositoryImpl.java
@@ -1,11 +1,14 @@
 package sesohaeng.sesohaengbackend.domain.place;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import sesohaeng.sesohaengbackend.dto.querydsl.place.GetPlaceListDto;
 import sesohaeng.sesohaengbackend.dto.querydsl.place.QGetPlaceListDto;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import java.util.List;
+
+import static sesohaeng.sesohaengbackend.domain.place.QPlace.place;
 
 public class PlaceRepositoryImpl implements PlaceRepositoryCustom{
 
@@ -19,11 +22,13 @@ public class PlaceRepositoryImpl implements PlaceRepositoryCustom{
     }
 
     @Override
-    public List<QGetPlaceListDto> searchPlaceByKeyword(String keyword) {
+    public List<GetPlaceListDto> searchPlaceByKeyword(String keyword) {
         return queryFactory
                 .select(new QGetPlaceListDto(
-                        QPlace.place.id,
-                        QPlace.place.placeName
-                ))
+                        place.id,
+                        place.placeName
+                )).from(place)
+                .where(place.placeName.contains(keyword))
+                .fetch();
     }
 }
--- src/main/java/sesohaeng/sesohaengbackend/dto/querydsl/place/GetPlaceListDto.java
+++ src/main/java/sesohaeng/sesohaengbackend/dto/querydsl/place/GetPlaceListDto.java
@@ -2,16 +2,15 @@ package sesohaeng.sesohaengbackend.dto.querydsl.place;
 
 
 import com.querydsl.core.annotations.QueryProjection;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
-public class QGetPlaceListDto {
+public class GetPlaceListDto {
     Long placeId;
     String placeName;
 
     @QueryProjection
-    public QGetPlaceListDto(Long placeId, String placeName) {
+    public GetPlaceListDto(Long placeId, String placeName) {
         this.placeId = placeId;
         this.placeName = placeName;
     }

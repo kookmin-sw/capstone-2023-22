b54863be3538d7c4a5d291e4c1440d2f040b006e
jin-jae <jinjae.dev@gmail.com>
2023-04-22 12:44:50 +0900

    add: map package

--- src/screens/MapScreen.tsx
+++ src/screens/MapScreen.tsx
@@ -5,16 +5,17 @@ import { FeedListItem } from '../components/FeedListItem';
 import { Header } from '../components/Header/Header';
 import { Spacer } from '../components/Spacer';
 import { useRootNavigation, useRootRoute } from '../navigations/RootStackNavigation';
+import WebView from 'react-native-webview';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import MapView from 'react-native-maps';
 
 export const MapScreen:React.FC = ()=>{
     const rootNavigation = useRootNavigation<'Map'>();
     const route = useRootRoute();
 
-    // const onPressBack = useCallback(()=>{
-    //     rootNavigation.goBack();
-    // }, [])
     return (
-        <View style={{flex:1}}>
-        </View>
+        <SafeAreaView>
+            <MapView></MapView>
+        </SafeAreaView>
     )
 }

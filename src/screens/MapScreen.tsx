6aa39aff1fa53c4046222fc63b708685e6d97fea
jin-jae <jinjae.dev@gmail.com>
2023-04-24 15:58:35 +0900

    feat: map page render success

--- src/screens/MapScreen.tsx
+++ src/screens/MapScreen.tsx
@@ -7,6 +7,8 @@ import { Spacer } from '../components/Spacer';
 import { useRootNavigation, useRootRoute } from '../navigations/RootStackNavigation';
 import WebView from 'react-native-webview';
 import { SafeAreaView } from 'react-native-safe-area-context';
+
+import { PROVIDER_GOOGLE } from 'react-native-maps';
 import MapView from 'react-native-maps';
 
 export const MapScreen:React.FC = ()=>{
@@ -14,8 +16,22 @@ export const MapScreen:React.FC = ()=>{
     const route = useRootRoute();
 
     return (
-        <SafeAreaView>
-            <MapView></MapView>
-        </SafeAreaView>
+        <>
+            <Header>
+                <Header.Group>
+                    <Header.Title title='50개 특구'></Header.Title>
+                </Header.Group>
+            </Header>
+            <MapView
+            provider={PROVIDER_GOOGLE}
+            style={{ flex: 1 }}
+            initialRegion={{
+            latitude: 37.541,
+            longitude: 126.986,
+            latitudeDelta: 0.5,
+            longitudeDelta: 0.5,
+            }}>
+            </MapView>
+        </>
     )
 }

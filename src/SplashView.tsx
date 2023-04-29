eba482b5d2817b9a7195b4be8c85a32a7761cc5b
jin-jae <jinjae.dev@gmail.com>
2023-04-29 20:35:59 +0900

    add: loading screen, application images

--- src/SplashView.tsx
+++ src/SplashView.tsx
@@ -1,15 +1,16 @@
 import React, { useCallback, useEffect } from 'react';
-import { View } from 'react-native';
+import { View, Image } from 'react-native';
 import { useDispatch } from 'react-redux';
 import { signIn, TypeUserDispatch } from './actions/user';
 import { Typography } from './components/Typography';
+import Splash from '../assets/splash.png';
 
 export const SplashView:React.FC<{onFinishLoad:()=>void}> = (props)=>{
     const dispatch = useDispatch<TypeUserDispatch>();
     const appInit = useCallback(async()=>{
         await dispatch(signIn());
         props.onFinishLoad();
-        
+
     }, [])
     useEffect(()=>{
         appInit();
@@ -17,7 +18,7 @@ export const SplashView:React.FC<{onFinishLoad:()=>void}> = (props)=>{
     }, [])
     return (
         <View style={{flex:1, alignItems:'center', justifyContent:'center'}}>
-            <Typography fontSize={48}>SPLASH VIEW</Typography>
+            <Image source={Splash} style={{width: 100 }} />
         </View>
     )
 }

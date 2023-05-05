575741000e402eff5fb995a0827dd7b878b1f4ff
yuujinkim <yuuuuu01@naver.com>
2023-05-05 17:23:00 +0900

    feat: 내가 쓴 게시물 가져오기 API

--- src/main/java/sesohaeng/sesohaengbackend/domain/feed/FeedRepository.java
+++ src/main/java/sesohaeng/sesohaengbackend/domain/feed/FeedRepository.java
@@ -1,6 +1,7 @@
 package sesohaeng.sesohaengbackend.domain.feed;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import sesohaeng.sesohaengbackend.domain.user.User;
 
 import java.util.List;
 import java.util.Optional;
@@ -9,6 +10,7 @@ public interface FeedRepository extends JpaRepository<Feed,Long> {
     Feed save(Feed feed);
     Optional<Feed> findById(Long id);
     List<Feed> findAll();
+    List<Feed> findByUser(User user);
     @Override
     void deleteById(Long id);
 }

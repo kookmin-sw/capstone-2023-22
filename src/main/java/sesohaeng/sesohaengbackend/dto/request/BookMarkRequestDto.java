d208ea7d29ff39bf2e0aa0b7008d4787909ebb2a
SeYeol00 <dmot0126@gmail.com>
2023-05-12 22:19:19 +0900

    fix: json 파싱 에러 해결

--- src/main/java/sesohaeng/sesohaengbackend/dto/request/BookMarkRequestDto.java
+++ src/main/java/sesohaeng/sesohaengbackend/dto/request/BookMarkRequestDto.java
@@ -2,9 +2,11 @@ package sesohaeng.sesohaengbackend.dto.request;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.RequiredArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@RequiredArgsConstructor
 public class BookMarkRequestDto {
 
     Long placeId;

534c3508364636bb82728f0563193cd816fcb412
halfmbbn <halfmbbn@gmail.com>
2023-05-13 15:35:01 +0900

    로그인 로직 미완성

--- src/RootApp.tsx
+++ src/RootApp.tsx
@@ -1,13 +1,30 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { NavigationContainer } from '@react-navigation/native';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { getUserInfo, signIn, TypeUserDispatch } from './actions/user';
 import { RootStackNavigation } from './navigations/RootStackNavigation';
 import { SplashView } from './SplashView';
+import { useDispatch } from 'react-redux';
+import { LoginScreen } from './screens/LoginScreen';
+
 
 export const RootApp:React.FC = ()=>{
-    const [initialize, setInitialize] = useState(false);
-
-    if(!initialize){
-        return <SplashView onFinishLoad={()=> setInitialize(true)}/>
+    const [initialize, setInitialize] = useState(false)
+    const dispatch = useDispatch<TypeUserDispatch>();
+    
+    useEffect(() => {
+        removeStorage('@token');
+        getStorage('@token').then((token) => {
+        if (token) {
+            getUserInfo();
+        } else {
+            console.log("no token");
+        }
+        })
+      }, []);
+    
+    if (!initialize) {
+        return <LoginScreen />
     }
 
     return (
@@ -15,4 +32,19 @@ export const RootApp:React.FC = ()=>{
             <RootStackNavigation/>
         </NavigationContainer>
     )
-}
+};
+// get
+export const getStorage = async (key:string) => {
+    const result = await AsyncStorage.getItem(key);
+    return result && JSON.parse(result);
+  };
+  
+  // set
+  export const setStorage = async (key:string, value:any) => {
+    return await AsyncStorage.setItem(key, JSON.stringify(value));
+  };
+  
+  // remove
+  export const removeStorage = async (key:string) => {
+    return await AsyncStorage.removeItem(key);
+  };
--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -3,17 +3,20 @@ import { useRootNavigation, useRootRoute } from '../navigations/RootStackNavigat
 import { Image, TouchableOpacity, View } from 'react-native';
 import * as WebBrowser from "expo-web-browser";
 import * as Google from "expo-auth-session/providers/google";
-
+import {setUserInfo, TypeUserDispatch } from '../actions/user';
 import googleLogin from "../../assets/google-login.png";
 import { EXPO_CLIENT_ID, EXPO_IOS_ID } from '../secrets';
 import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { Config } from '../config';
+import { useDispatch } from 'react-redux';
 
 WebBrowser.maybeCompleteAuthSession();
 
 export const LoginScreen:React.FC = () => {
     const rootNavigation = useRootNavigation<'Login'>();
     const route = useRootRoute();
+    const dispatch = useDispatch<TypeUserDispatch>();
 
     const [request, response, promptAsync] = Google.useAuthRequest({
         expoClientId: EXPO_CLIENT_ID,
@@ -29,8 +32,15 @@ export const LoginScreen:React.FC = () => {
           console.log("received token:", gotToken.data.data.accessToken);
           AsyncStorage.setItem('@token', gotToken.data.data.accessToken);
           axios.defaults.headers.common['Authorization'] = `Bearer ${gotToken.data.data.accessToken}`;
-        })
-        .catch(function (error) {
+          axios.get(`${Config.server}/user`).then(async user => {
+            await AsyncStorage.setItem('user', JSON.stringify(user.data.data));
+            dispatch(setUserInfo({
+              "id": user.data.data.userId,
+              "name": user.data.data.userName,
+              "profileImage":user.data.profileImage,
+              "birth":"23-05-01"}));
+          })
+        }).catch(function (error) {
           console.log("ERROR:", error);
         });
     }

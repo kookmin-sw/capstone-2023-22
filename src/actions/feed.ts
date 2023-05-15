e790140fa46e5ec936d1ff60c3c898945e9bcf1a
jin-jae <jinjae.dev@gmail.com>
2023-05-15 13:33:41 +0900

    fix: delete base url const

--- src/actions/feed.ts
+++ src/actions/feed.ts
@@ -5,8 +5,6 @@ import { sleep } from "../utils/utils";
 import axios from "axios";
 import { Config } from "../config";
 
-const BASE_URL = 'http://127.0.0.1:8080' 
-
 export const GET_FEED_LIST_REQUEST = 'GET_FEED_LIST_REQUEST' as const;
 export const GET_FEED_LIST_SUCCESS = 'GET_FEED_LIST_SUCCESS' as const;
 export const GET_FEED_LIST_FAILURE = 'GET_FEED_LIST_FAILURE' as const;
@@ -80,19 +78,19 @@ export const getFeedList = ():FeedListThunkAction=> async (dispatch)=>{
     dispatch(getFeedListRequest());
     axios.get(`${Config.server}/posts`).then(res => {
         console.log(res.data.data.feeds);
-        // TODO: 피드 좋아요 상태 조회 추가 
+        // TODO: 피드 좋아요 상태 조회 추가
         // const feedList:FeedInfo[] = res.data.data.feeds;
         // const feedsWithLike:FeedInfo[] = feedList.map(f => {
         //     axios.get(`${Config.server}/posts/${f.id}/heart`)
         //     .then(res_like => {return res_like.data.data})
         //     .catch(err_like => console.log(err_like));
-        //     return 
+        //     return
         // });
         // console.log(feedsWithLike);
         dispatch(
-            getFeedListSuccess(res.data.data.feeds))    
+            getFeedListSuccess(res.data.data.feeds))
     }).catch(err => {console.log(err.response)});
-    
+
 }
 
 export const createFeed = (item:Omit<FeedInfo, 'id'|'writer'|'likeCount'>):FeedListThunkAction => async (dispatch, getState)=>{
@@ -120,8 +118,8 @@ export const favoriteFeed = (item:FeedInfo):FeedListThunkAction => async (dispat
 
 export type FeedListThunkAction = ThunkAction<void, RootReducer, undefined, FeedListActions>;
 export type TypeFeedListDispatch = ThunkDispatch<RootReducer, undefined, FeedListActions>;
-export type FeedListActions = 
-    | ReturnType<typeof getFeedListSuccess> 
+export type FeedListActions =
+    | ReturnType<typeof getFeedListSuccess>
     | ReturnType<typeof getFeedListRequest>
     | ReturnType<typeof getFeedListFailure>
     | ReturnType<typeof createFeedRequest>

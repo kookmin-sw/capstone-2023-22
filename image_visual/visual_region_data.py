4b32f4b884d701d7152a29d8b4bbdea3e4fbc2ae
sunho1999 <wmfrlek99@gmail.com>
2023-05-18 21:44:44 +0900

    각 지역별 이미지 가시화 수정 작업 진행 완료

--- image_visual/visual_region_data.py
+++ image_visual/visual_region_data.py
@@ -1,18 +1,20 @@
 import pandas as pd
-import numpy as np
 from datetime import datetime, timedelta
-import holidays
 import matplotlib.pyplot as plt
-import matplotlib
 import boto3
-from matplotlib import rc
 import os
-
+import io
+from matplotlib import rc, font_manager
 
 s3 = boto3.client('s3', aws_access_key_id="{HIDDEN}", aws_secret_access_key="{HIDDEN}")
 
+# S3 버킷 및 파일 경로
+bucket_name = 'mlops-api-output'
+file_key = '2023/2023.csv'
 
-df = pd.read_csv("/Users/sunho99/PycharmProjects/python_Project/캡스톤디자인/2023.csv")
+# CSV 파일 다운로드 및 읽기
+response = s3.get_object(Bucket=bucket_name, Key=file_key)
+df = pd.read_csv(io.BytesIO(response["Body"].read()))
 df_index_list = df.index
 df.insert(0,"지역",df_index_list)
 df_reset= df.reset_index(drop=True)
@@ -74,7 +76,8 @@ data = {'region': local_list,
 df = pd.DataFrame(data)
 
 for i in local_list:
-    plt.clf() # plt 초기화
+
+
     local_df = df["region"] == i  # "지역" column에서 "지역 정보" 값을 가지고 있는 행 추출
     filtered_local_df = df[local_df]
     labels = filtered_local_df.columns[1:3]
@@ -84,13 +87,18 @@ for i in local_list:
     colors = ['#ffc000', '#d395d0']
     explode = (0.05, 0.05)  # 조각을 돌출시킬 값
     wedgeprops = {'width': 0.5, 'edgecolor': 'w', 'linewidth': 5}
+    label =["남성","여성"]
 
-    # 그래프 그리기
-    plt.pie(values, autopct='%.1f%%', startangle=260, counterclock=False, explode=explode, shadow=True, colors=colors)
-    plt.legend(labels)
+    plt.rcParams["font.family"] = 'NanumGothic'  # 한글 폰트 적용
+
+    # 그래프 그리기 인구 성비 원형 파이
+    plt.pie(values, labels=label,autopct='%.1f%%', startangle=260, counterclock=False, explode=explode, shadow=True, colors=colors)
+    plt.legend(label)
+    plt.title("%s 남녀 성비율" % i,weight='bold',fontsize=10)
     plt.axis('equal')  # 원을 원형으로 유지
-    plt.savefig("%s_rate_sex.png"%i) # 각 구에 맞는 성 비율 이미지
-
+    plt.axis('off')
+    plt.savefig("%s_rate_sex.png"%i,pad_inches=0) # 각 구에 맞는 성 비율 이미지
+    # plt.show()
     output_filename = "%s_rate_sex.png"%i
     file_name = output_filename
 
@@ -101,26 +109,35 @@ for i in local_list:
     # 로컬 파일을 S3에 업로드합니다.
     s3.upload_file(file_path, bucket_name, object_key)
 
-
+    plt.close()
     plt.clf() # plt 초기화
 
 
     # 데이터 준비
     rate_column = filtered_local_df[['rate_10', 'rate_20', 'rate_30', 'rate_40', 'rate_50']].values
-    x = ['rate_10', 'rate_20', 'rate_30', 'rate_40', 'rate_50']
+    x = ['20대 미만', '20대', '30대', '40대', '50대 이상']
     rate_values = [rate_column[0,0],rate_column[0,1],rate_column[0,2],rate_column[0,3],rate_column[0,4]]
     # 최고값 찾기
     max_value = max(rate_values)
     max_index = rate_values.index(max_value)
 
-    # 최고값 표시
-    plt.text(max_index, max_value, str(round(max_value,1))+"%", ha='center', va='bottom')
-
+    plt.rcParams["font.family"] = 'NanumGothic'  # 한글 폰트 적용
+    colors = ['#ffc000', '#d395d0','#CCCDF5','#CDF5CC','#F4CCF5']
     # 막대 그래프 그리기
-    plt.bar(x, rate_values)
-    # x 축 틱과 레이블 숨기기
-    plt.xticks([])
+    x = plt.bar(x, rate_values,color=colors)
+
+    # 최고값 표시
+    plt.text(max_index, max_value, str(round(max_value,1))+"%", ha='center', va='bottom',weight='bold',fontsize=8,color='black')
+
+    # 크기 표시하기
+    for p in x.patches:
+        left, bottom, width, height = p.get_bbox().bounds
+        plt.annotate(f"{str(int(height))+'%'}", (left + width / 2, height * 0.5), ha='center', size=10, color='#E6F0F8')
+    plt.title("%s 나이대 비율" % i,weight='bold',fontsize = 10)
     plt.savefig("%s_rate_old.png" % i) # 각 구에 맞는 나이 비율 이미지
+    plt.close()
+    plt.clf() # plt 초기화
+
 
     output_filename = "%s_rate_old.png" % i
     file_name = output_filename
